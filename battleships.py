a5d8ac6626c6a59d06c65d798b1d01e6ade98dec
Alex <alexedgibbs@gmail.com>
2019-08-13 21:35:43 +0100

    WE MADE DUR SHIPS COORDINATE

--- battleships.py
+++ battleships.py
@@ -9,21 +9,39 @@ def make_board(grid_size):
     return board
             
 
-def place_ship(ship, board, coord):
+def place_ship(ship, board, x, y):
+    print(f'Direction: {direction}')
     print(f'Ship Length: {ship}')
     print(f'Coordinate: {coord}')
     return board
 
+def check_coordinate(coordinate, grid_size):
+    pass
+
+
 ship_girths = [2, 3, 4, 5]
 
-
+check_coord = True
 
 board = make_board(grid_size=10)
 for ship in ship_girths:
-    coord =  input('Please enter a coordinate: ')
-    place_ship(ship, board, coord)
-    
+    while check_coord:     
+        coord =  input('Please enter a coordinate, in format x, y: ')  
+        try:
+            coord = coord.replace(' ', '').split(',')
+            x = int(coord[0])
+            y = int(coord[1])
+        except Exception:
+            print('Invalid Coordinate')
+        else: 
+            place_ship(ship, board, x, y)  
+            check_coord = False
+    check_coord = True 
+        
+
 
 for line in board:
     print(line)
 
+
+    
