8d2753b40064ed8f29a7e4e922ca2466a193fc9a
Alex <alexedgibbs@gmail.com>
2019-08-14 16:50:53 +0100

    Made exception handling routines

--- battleships.py
+++ battleships.py
@@ -8,38 +8,48 @@ def make_board(grid_size):
                 
     return board
             
-
 def place_ship(ship, board, x, y):
-    print(f'Direction: {direction}')
+    # print(f'Direction: {direction}')
     print(f'Ship Length: {ship}')
     print(f'Coordinate: {coord}')
+    for i in range(ship):
+        if board[len(board) - (y - 2) - i][x - 1] == 'X':
+            raise ValueError
+
+    for i in range(ship):
+        board[len(board) - (y - 2) - i][x - 1] = 'X'
+            
     return board
 
-def check_coordinate(coordinate, grid_size):
-    pass
+def check_ship_len(x, y, ship_length, grid_size):
+    return not(y + ship_length - 1 > grid_size)
 
+def is_coord_on_board(x, y, grid_size):
+    if 1 <= x <= grid_size and 1 <= y <= grid_size:
+        pass 
+    else:
+        raise ValueError
 
-ship_girths = [2, 3, 4, 5]
-
+ship_lengths = [2, 3, 4, 5]
+grid_size = 10 
 check_coord = True
 
-board = make_board(grid_size=10)
-for ship in ship_girths:
+board = make_board(grid_size=grid_size)
+for ship in ship_lengths:
     while check_coord:     
         coord =  input('Please enter a coordinate, in format x, y: ')  
         try:
             coord = coord.replace(' ', '').split(',')
             x = int(coord[0])
             y = int(coord[1])
-        except Exception:
-            print('Invalid Coordinate')
-        else: 
+            is_coord_on_board(x=x, y=y, grid_size=grid_size)    
             place_ship(ship, board, x, y)  
+        except (IndexError, ValueError):
+            print('Invalid Coordinate\n')
+        else: 
             check_coord = False
     check_coord = True 
         
-
-
 for line in board:
     print(line)
 
